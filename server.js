b611cf3521a8f37fc250bc2bb09079aec258d7f5
Dave Bouwman <dave@davebouwman.com>
2018-07-11 16:19:06 -0700

    root for google

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use('/', express.static('apps/exb'));
 app.use('/js', express.static('apps/exb'));
 
 app.get('/', function(req, res) {
-  res.redirect(301, 'https://radar-app.now.sh/exb?id=8261ae335d2044eb9837cb858847c692');
+  res.render('root');
+  // res.redirect(301, 'https://radar-app.now.sh/exb?id=8261ae335d2044eb9837cb858847c692');
 })
 
 // now the route to handle the exb index file...
